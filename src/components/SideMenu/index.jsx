9045e883fc26bfc1890aa766143ce78a8f77fc65
WesleyR10 <wesleyribas2015@gmail.com>
2023-12-04 13:39:21 -0300

    inserting singOut mobile

--- src/components/SideMenu/index.jsx
+++ src/components/SideMenu/index.jsx
@@ -8,12 +8,17 @@ import { USER_ROLE } from '../../../utils/roles';
 import { useNavigate } from "react-router-dom";
 
 export function SideMenu({ menuIsOpen, onCloseMenu }) {
-  const { user } = useAuth();
+  const { signOut, user } = useAuth();
   const navigate = useNavigate()
 
   function goCreateProduct() {
     navigate("/new")
   }
+
+  function handleSignOut() {
+    signOut()
+  }
+
   return (
     <Container data-menu-is-open={menuIsOpen}>
       <Header>
@@ -24,7 +29,7 @@ export function SideMenu({ menuIsOpen, onCloseMenu }) {
       <Content>
         <SearchInput />
         {user.role === USER_ROLE.ADMIN ? <>
-          <h1 onClick={goCreateProduct}>Novo Prato</h1> <h1 onClick={onCloseMenu}>Sair</h1>
+          <h1 onClick={goCreateProduct}>Novo Prato</h1> <h1 onClick={handleSignOut}>Sair</h1>
         </> :
           <h1 onClick={onCloseMenu}>Sair</h1>
         }
